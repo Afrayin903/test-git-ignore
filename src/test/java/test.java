847b3602bf55256353d5d9cfa57754fa9d873cad
Kahar <afrayin0222@gmail.com>
2025-11-20 10:04:33 -0500

    add new files

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,5 +1,8 @@
 public class test {
     public static void main(String[] args) {
+
         System.out.println("Testing git merge");
+        System.out.println("Created multiple branch for merge each other and merge to main");
+        System.out.println("Merge to branches one another");
     }
 }
