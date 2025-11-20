ce970239a49749b9d3681021b776ea34d237deec
Kahar <afrayin0222@gmail.com>
2025-11-20 11:32:26 -0500

    delete

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -3,7 +3,6 @@ package org.example;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("test git merge");
         System.out.println("Practice merge features");
         System.out.println("Merged");
         System.out.println("Unsuccessful");
